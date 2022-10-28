f893936863c18bf5ad6ef7abfcca43bc64d924b4
kinyaaa <r202201953vr@jindai.jp>
2022-10-28 16:49:12 +0900

    2022/10/28

---
+++ de12/python/dep1.py
@@ -0,0 +1,17 @@
+import random
+import copy
+from select import select
+from tkinter.messagebox import QUESTION
+print("母と子")
+print("あなたは、一児の母として、子育てしつつ、家事もこなす専業主婦です。"
+"")
+print("しかし、数字を選ぶ際、必ず相手より大きな数字を選ばなければなりません。自分の数字に相手より大きな数字がない場合パスになります。")
+print("パスの場合は０を入力してください")
+
+print("息子「ただいま」")
+select = int(input("あなた　1.「おかえり、ケーキ買ってるよ」" "2.「おかえり、テストどうだった？」"))
+if selecta ==1:
+    print("今お腹空いてないから、後で食べる")
+if selecta ==2:
+    print("まあまあ")
+
